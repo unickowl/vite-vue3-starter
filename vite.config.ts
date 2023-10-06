e17ddd88eaf68a1369dbbfadf98daf940c280333
nick_lin <nick_lin@owlting.com>
2023-10-06 12:09:55 +0800

    💡 feat: add auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,15 @@ export default defineConfig({
     vue(),
     vueJsx(),
     VueDevTools(),
-    UnoCSS()
+    UnoCSS(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'pinia',
+        'vue-i18n'
+      ]
+    })
   ],
   resolve: {
     alias: {
