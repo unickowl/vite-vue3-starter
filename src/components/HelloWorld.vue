e060a528030f18d0959d1b095c1d01ee1adbd58b
nick_lin <nick_lin@owlting.com>
2023-10-06 11:53:58 +0800

    💡 feat: add vueuse

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
+import { useLocalStorage, useMouse } from '@vueuse/core'
+
 defineProps<{
   msg: string
 }>()
 
 const { t, locale } = useI18n()
-
+const storeLocale = useLocalStorage(import.meta.env.VITE_APP_LOCALE_KEY, 'en')
 function handleLocaleChange(lang: string) {
   locale.value = lang
+  storeLocale.value = lang
 }
+
+const { x, y } = useMouse()
 </script>
 
 <template>
@@ -20,6 +25,9 @@ function handleLocaleChange(lang: string) {
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
+    <div class="border-solid my-4 p-4 text-center">
+      Mouse: {{ x }}, {{ y }}
+    </div>
     <div class="flex gap-4 my-4">
       <button @click="handleLocaleChange('en')">En</button>
       <button @click="handleLocaleChange('zh_tw')">zhTW</button>
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,9 +1,12 @@
 import { createI18n } from 'vue-i18n'
 import en from './locales/en.json'
 import zh_tw from './locales/zh_tw.json'
+import { useLocalStorage } from '@vueuse/core'
+
+const storeLocale = useLocalStorage(import.meta.env.VITE_APP_LOCALE_KEY, 'en')
 
 const i18n = createI18n({
-  locale: 'en',
+  locale: storeLocale.value,
   legacy: false,
   fallbackLocale: 'en',
   messages: {
